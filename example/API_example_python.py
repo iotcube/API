6fa79857c9e168d0df830994878d45f8e4986687
WOOSEUNGHOON <seunghoonwoo@korea.ac.kr>
2019-09-04 11:16:16 +0900

    Update API_example_python.py

--- example/API_example_python.py
+++ example/API_example_python.py
@@ -1,5 +1,5 @@
 import requests
 
 files = {'file': ("/filepath/to/hidxfile.hidx", open("/filepath/to/hidxfile.hidx", 'rb'))}
-headers = {'user-agent': 'your user-agent value'
+headers = {'user-agent': 'your user-agent value'}
 response = requests.post("https://iotcube.net/api/wf1", files=files, headers=headers)
